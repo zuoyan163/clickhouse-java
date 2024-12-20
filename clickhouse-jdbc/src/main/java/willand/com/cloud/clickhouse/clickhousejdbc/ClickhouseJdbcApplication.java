5a13618317887afdfc9edd227fa6b07af6395d96
guilin.lgl <guilin.lgl@ninebot.com>
2024-12-20 20:06:53 +0800

    合并代码和创建工程

---
+++ clickhouse-jdbc/src/main/java/willand/com/cloud/clickhouse/clickhousejdbc/ClickhouseJdbcApplication.java
@@ -0,0 +1,13 @@
+package willand.com.cloud.clickhouse.clickhousejdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickhouseJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickhouseJdbcApplication.class, args);
+    }
+
+}
---
+++ clickhouse-jdbc/src/test/java/willand/com/cloud/clickhouse/clickhousejdbc/ClickhouseJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package willand.com.cloud.clickhouse.clickhousejdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickhouseJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
